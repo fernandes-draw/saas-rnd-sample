25d64f188569ae7dc91ed78a582ab3aa464ea625
fernandes-draw <fernandes_draw@hotmail.com>
2026-09-22 12:13:26 -0300

    Prepare for Railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = "django-insecure-o%_@o6=%x5(x-305k#gt^4eicyu5(za8s+ky%z1ki+)9c&!lox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app",  # https://saas.prod.railway.app
+]
 
+if DEBUG:
+    ALLOWED_HOSTS += ["127.0.0.1", "localhost"]
 
 # Application definition
 
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,35 +6,27 @@ from visits.models import PageVisits
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     qs = PageVisits.objects.all()
     page_qs = PageVisits.objects.filter(path=request.path)
+
+    try:
+        percent = ((page_qs.count() * 100.0) / qs.count(),)
+    except:
+        percent = 0
 
     my_title = "My Page"
     html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "percent": percent,
         "total_visit_count": qs.count(),
     }
     PageVisits.objects.create(path=request.path)
 
     return render(request, html_template, my_context)
-
-
-def old_home_page_view(request, *args, **kwargs):
-    my_title = "My Page"
-    my_context = {"page_title": my_title}
-    html_ = """
-<!DOCTYPE html>
-<html>
-  <head></head>
-  <body>
-    <h1>{page_title} anything?</h1>
-  </body>
-</html>
-""".format(**my_context)
-    # html_file_path = this_dir / "home.html"
-    # html_ = html_file_path.read_text()
-    return HttpResponse(html_)
