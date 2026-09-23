bbf6454e763ac690147b37276dea751a8dbef51f
fernandes-draw <fernandes_draw@hotmail.com>
2026-09-22 22:31:39 -0300

    Prepare static files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -91,17 +91,18 @@ DATABASES = {
     }
 }
 
-CONN_MAX_AGE= config("CONN_MAX_AGE", cast=int, default=30)
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
 DATABASE_URL = config("DATABASE_URL", cast=str)
 
 if DATABASE_URL is not None:
     import dj_database_url
+
     DATABASES = {
         "default": dj_database_url.config(
-            default=DATABASE_URL,
+            default=DATABASE_URL,  # type: ignore
             conn_max_age=CONN_MAX_AGE,
             conn_health_checks=True,
-            )
+        )
     }
 
 
@@ -140,6 +141,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [STATICFILES_BASE_DIR]
+
+# output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR / "local-cdn"
+
+# if not DEBUG:
+#     STATIC_ROOT = BASE_DIR / "prod-cdn"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
