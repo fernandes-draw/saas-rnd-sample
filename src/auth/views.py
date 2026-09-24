544f7c088ccd184e215f02199e86fcd92705d4ee
fernandes-draw <fernandes_draw@hotmail.com>
2026-09-23 21:12:23 -0300

    4 - HTML Login Form and Template

--- src/auth/views.py
+++ src/auth/views.py
@@ -4,14 +4,16 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username") or None
+        password = request.POST.get("password") or None
 
-    username = "everson"  # request.POST["username"]
-    password = "Ev1629mA@"  # request.POST["password"]
-    user = authenticate(request, username=username, password=password)
+        if all([username, password]):
+            user = authenticate(request, username=username, password=password)
 
-    if user is not None:
-        login(request, user)
-        print("Login here!")
-        return redirect("/")
+            if user is not None:
+                login(request, user)
+                print("Login here!")
+                return redirect("/")
 
     return render(request, "auth/login.html", {})
